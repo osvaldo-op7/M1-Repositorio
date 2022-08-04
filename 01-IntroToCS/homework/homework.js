d7eb001571296e8ae5d5f17609b906eba481e268
osvaldo-op7 <108706136+osvaldo-op7@users.noreply.github.com>
2022-08-03 20:54:27 -0500

    Second commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,14 +2,26 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var con = 0;
+  var resultado = 0;
+  for(let i=num.length-1;i>=0;i--){
+    resultado += (num[i] * Math.pow(2,con));
+    con++; 
+  }
+  return resultado;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var resultado = '';
+  var conver = '';
+  while(num != 0){
+    conver = (num % 2).toString();
+    resultado = conver + resultado;  
+    num = parseInt(num/2);
+  }
+  return resultado;
 }
-
 
 module.exports = {
   BinarioADecimal,
