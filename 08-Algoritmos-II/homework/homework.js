10e62f1dc0c18580c5ca5fc3b11011cbd94b8574
osvaldo-op7 <108706136+osvaldo-op7@users.noreply.github.com>
2022-08-28 18:51:42 -0500

    commit prox

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,16 +6,47 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array;
+  }
 
-}
+  var pivot = array[0];
+  
+  var left = []; 
+  var right = [];
+
+  for (var i = 1; i < array.length; i++) {
+    array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+  }
+
+  return quickSort(left).concat(pivot, quickSort(right));
+  }
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if (array.length <= 1) return array
+  let mid = Math.floor(array.length / 2)
+  let left = mergeSort(array.slice(0, mid))
+  let right = mergeSort(array.slice(mid))
+  return merge(left, right)
 }
+function merge(left, right) {
+  let arr = []
+  
+  while (left.length && right.length) {
+      
+      if (left[0] < right[0]) {
+          arr.push(left.shift())  
+      } else {
+          arr.push(right.shift()) 
+      }
+  }
+  return [ ...arr, ...left, ...right ]
+}
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
